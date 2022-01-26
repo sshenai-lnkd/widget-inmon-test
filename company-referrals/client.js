34b56d835c1656938e5a4e87ab2a652ab197c1b4
Sameer Shenai <sshenai@sshenai-mn3.linkedin.biz>
2022-01-25 18:01:58 -0800

    CC fix

--- company-referrals/client.js
+++ company-referrals/client.js
@@ -11,42 +11,48 @@
 /* global IN */
 IN.tags.add('CompanyConnections', function CompanyConnections(node, core) {
     let tag = new IN.SDK.Tag(node, core);
-  
+
     // Validate client parameters
     const integrationContext = tag.attributes["integration-context"];
-  
+
     if (!integrationContext) {
-      throw new Error(
-          "Applicant Highlights widget requires an integration-context"
-      );
+        throw new Error(
+            "Applicant Highlights widget requires an integration-context"
+        );
     }
 
     let iframe = new IN.SDK.EmbeddedWindow('https://sshenai-lnkd.github.io/widget-inmon-test/company-referrals/server.html', {
-      attributes: {
-        width: 420,
-        height: 650,
-      },
-      method: 'GET'
+        attributes: {
+            width: 420,
+            height: 650,
+        },
+        method: 'GET'
     });
     tag.insert(iframe.self);
     const client = new IN.SDK.Client(iframe);
-   
+
     // fire the xdoor 'refresh' event to reload the page on successful authentication
     client.on('ready', () => {
-        client.on('refer', function() {
-            let modalWindow  = new IN.SDK.EmbeddedWindow('https://sshenai-lnkd.github.io/widget-inmon-test/company-referrals/referModal.html', {
-             attributes: {
-               width: 420,
-               height: 650,
-             },
-             method: 'GET'
-               });
-             tag.insert(modalWindow.self);
-           });
+        client.on('refer', () => {
+            let modalWindow = new IN.SDK.EmbeddedWindow('https://sshenai-lnkd.github.io/widget-inmon-test/company-referrals/referModal.html', {
+                attributes: {
+                    width: 420,
+                    height: 650,
+                },
+                method: 'GET'
+            });
+            tag.insert(modalWindow.self);
+        });
     });
-
-   
-  
+    client.on('refer', () => {
+        let modalWindow = new IN.SDK.EmbeddedWindow('https://sshenai-lnkd.github.io/widget-inmon-test/company-referrals/referModal.html', {
+            attributes: {
+                width: 420,
+                height: 650,
+            },
+            method: 'GET'
+        });
+        tag.insert(modalWindow.self);
+    });
     return tag;
-  });
-  
+});
